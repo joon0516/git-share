043332a8cae1fc649e1d86df5565658bd8288917
neskech <neskechastro@gmail.com>
2024-01-09 21:21:58 -0500

    tableeee

--- src/components/fileTree.tsx
+++ src/components/fileTree.tsx
@@ -6,6 +6,7 @@ import { type Option } from '~/utils/option';
 import { useQuery } from '@tanstack/react-query';
 import { assert } from '~/utils/contract';
 import { useState } from 'react';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './shadcn/table';
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -20,6 +21,8 @@ interface FileTreeProps {
   repository: string;
   owner: string;
   branch: Option<string>;
+  width: string;
+  height: string;
   onItemSelected: (itemPath: string) => void;
 }
 
@@ -40,20 +43,27 @@ function FileTree(props: FileTreeProps) {
   if (data.isErr()) return <p> There was an error... {data.unwrapErr()} </p>;
 
   return (
-    <table className="table-auto">
-      <tbody>
+    <Table className='rounded-sm border-separate'>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="h-[50px]r w-[100px] bg-slate-800 text-center text-slate-300">
+            Files
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
         {data?.unwrap().map((e) => (
-          <tr key={e.path}>
-            <td
+          <TableRow key={e.path} className='border-t-0'>
+            <TableCell
               onClick={(_) => setSelection(e.path)}
               className={tableColumnStyles(e.path, selectedItem)}
             >
-              {responseItemToLabelIcon({ item: e })}{' '}
-            </td>
-          </tr>
+              {responseItemToLabelIcon({ item: e })}
+            </TableCell>
+          </TableRow>
         ))}
-      </tbody>
-    </table>
+      </TableBody>
+    </Table>
   );
 }
 
@@ -88,7 +98,7 @@ interface IconProps {
 
 function responseItemToLabelIcon(props: IconProps) {
   return (
-    <div className="flex flex-row items-center gap-3 bg-slate-800 p-3 text-slate-100 select-none">
+    <div className="font-outfit flex h-[100%] select-none flex-row items-center gap-3 rounded-md p-3 text-sm text-slate-400">
       <FontAwesomeIcon icon={props.item.type == 'file' ? faFile : faFolder} />
       {props.item.name}
     </div>
@@ -97,9 +107,9 @@ function responseItemToLabelIcon(props: IconProps) {
 
 function tableColumnStyles(currentItem: string, selectedItem: string | null): string {
   const isSelected = currentItem == selectedItem;
-  const darkenIfSelected = isSelected ? 'brightness-[.25]' : '';
-  const darkenIfHover = !isSelected ? 'brightness-75' : '';
-  return `${darkenIfSelected} border-[0.5px] border-slate-600 p-0 hover:${darkenIfHover}`;
+  if (isSelected)
+    return 'h-[100%] border-[0.5px] border-slate-600 p-0 brightness-[0.55] bg-slate-900 border-t-0';
+  return 'border-[0.5px] border-slate-600 p-0 hover:brightness-75 bg-slate-900 border-t-0';
 }
 
 async function fetchDirectory(options: FileTreeProps): Promise<Result<DirectoryResponse, string>> {
